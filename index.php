2d399d5fccdd28a47432b0bb164170360e28a2a0
Srinivasa Prasad Ghantasala <prasad@bluplateau.com>
2018-03-28 04:43:09 +1300

    Added index.php page

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+	// Including head file
+	include("assets/php/globals/head.php");
+?>
+<body>
+
+	<?php
+		// Including header file
+		include("assets/php/globals/header.php");
+    ?>
+    
+    <!-- Start: Main Section -->
+
+    <!-- =========================
+     HEADER SLIDER
+============================== -->
+<div id="slider" class="slider">
+	<div class="sp-slides"> 
+    
+		<!-- HEADER SLIDER ITEM -->
+		<div class="sp-slide"> <img class="sp-image" src="assets/images/blank.gif" alt="" data-src="#" />
+			<div class="container">
+				<h2 class="sp-layer slider-subtitle" 
+									data-position="leftCenter" data-horizontal="15" data-vertical="-120" 
+									data-show-transition="left" data-hide-transition="left"
+									data-show-delay="400" data-hide-delay="400"> <b>FREE WINZ QUOTE</b> </h2>
+				<h2 class="sp-layer slider-subtitle" 
+									data-position="leftCenter" data-horizontal="15" data-vertical="-120" 
+									data-show-transition="left" data-hide-transition="left"
+									data-show-delay="400" data-hide-delay="400"> <b>FREE ACC TREATMENTS</b> </h2>
+				<h2 class="sp-layer slider-subtitle" 
+									data-position="leftCenter" data-horizontal="15" data-vertical="-120" 
+									data-show-transition="left" data-hide-transition="left"
+									data-show-delay="400" data-hide-delay="400"> <b>Free INITIAL CONSULTATION</b> </h2>
+				<h2 class="sp-layer slider-subtitle" 
+									data-position="leftCenter" data-horizontal="15" data-vertical="-120" 
+									data-show-transition="left" data-hide-transition="left"
+									data-show-delay="400" data-hide-delay="400"> <b>FREE TREATMENT UNDER 18</b> </h2> <br><br><br>
+				<h1 class="sp-layer slider-title" 
+									data-position="leftCenter" data-horizontal="15" data-vertical="0" 
+									data-show-transition="left" data-hide-transition="left"
+									data-show-delay="200" data-hide-delay="200">OPEN 7 DAYS</h1>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- =========================
+     END HEADER SLIDER
+============================== --> 
+
+    <!-- End: Main Section -->
+
+	<?php
+		// Including footer file
+		include("assets/php/globals/footer.php");
+	?>
+
+	<?php
+		// Including foot file
+		include("assets/php/globals/foot.php");
+	?>
+</body>
+</html>
