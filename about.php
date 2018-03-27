793f44fb1d8fdd97a1d51d07ba460770722beb36
Srinivasa Prasad Ghantasala <prasad@bluplateau.com>
2018-03-28 00:20:00 +1300

    Added about.php page & updated the content

---
+++ about.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+	// Including head file
+	include("assets/php/globals/head.php");
+?>
+<body>
+
+	<?php
+		// Including header file
+		include("assets/php/globals/header.php");
+    ?>
+    
+    <!-- Start: Main Section -->
+    <div class="page-title page-title-about bg-pattern" data-bgcolor="5295BD">
+        <div class="page-title-overlay">
+            <div class="container">
+            <div class="row">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-home"><a href="index.php"><i class="fa fa-home"></i></a></li>
+                    <li class="active">About Us</li>
+                </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <div class="history">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-5 col-md-5 col-sm-5 col-xs-12 history-images">
+                    <div class="image-carousel">
+                        <div class="owl-image-carousel" id="owl-image-carousel">
+                            <div class="image-carousel-item"> <img src="assets/images/about/about_01.jpg" alt="" /> </div>
+                        </div>
+                        <!-- Uncomment the previous buttons if there's more than 1 image -->
+                        <!-- 
+                            <div class="carousel-btn carousel-next" id="next-image-carousel"><i class="fa fa-angle-right"></i></div>
+                            <div class="carousel-btn carousel-prev" id="prev-image-carousel"><i class="fa fa-angle-left"></i></div>
+                        -->
+                    </div>
+                </div>
+                <div class="col-lg-7 col-md-7 col-sm-7 col-xs-12 history-text">
+                    <h3>On behalf of the Stoddard Dental Square office, Shivam welcomes you!</h3>
+                    <p>On this website, you will find a plenty of information about dentistry and procedures we offer at Stoddard Dental Square.We always believe in educating the patient about the treatment they received from us. This helps them to take significant, well informed decisions regarding their oral health care and treatment options. Therefore, we always encourage our patients to learn and explore as much about our services and dentistry as they wish. Within a short span of time, Stoddard Dental Square has established itself as a trustworthy brand among kiwis in Auckland because we provide highly skilled dental care for patients of all age groups within their budget. We always strive to provide dental treatment in a warm and relaxing environment. We accentuate a preventive philosophy. Shivam takes time with each of his patients, gives attention and understands the concerns of the respective individual.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End: Main Section -->
+
+	<?php
+		// Including footer file
+		include("assets/php/globals/footer.php");
+	?>
+
+	<?php
+		// Including foot file
+		include("assets/php/globals/foot.php");
+	?>
+</body>
+</html>
