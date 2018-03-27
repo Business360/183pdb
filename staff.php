c094b1beec537999f2dd4436b1db42ae9be4a8e8
Srinivasa Prasad Ghantasala <prasad@bluplateau.com>
2018-03-28 00:00:56 +1300

    Added staff.php page

---
+++ staff.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+	// Including head file
+	include("assets/php/globals/head.php");
+?>
+<body>
+
+	<?php
+		// Including header file
+		include("assets/php/globals/header.php");
+    ?>
+    
+    <!-- Start: Main Section -->
+    <div class="page-title page-title-doctor bg-pattern" data-bgcolor="2BAAA1">
+        <div class="page-title-overlay">
+            <div class="container">
+            <div class="row">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-home"><a href="index.php"><i class="fa fa-home"></i></a></li>
+                    <li class="active">Our Staff</li>
+                </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="doctors-text">
+        <div class="container">
+            <div class="row">
+
+                <h2 class="section-title text-center">
+                    <span class="bold700">
+                    Friendly Staff
+                    </span>
+                </h2>
+                
+                <!-- Sample 01 -->
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 single-staff-panel">
+                    <div class="doctors-text-doctor">
+                        <img src="assets/images/staff/staff-thumbnail.jpg" alt="" />
+                        <div class="doctors-text-doctor-name">Wyatt Esmond</div>
+                        <div class="doctors-text-doctor-position"> Chief of Dentists </div>
+                    </div>
+                    <div class="doctors-text-right">
+                    <p>Our Dental Clinic is dedicated to providing the most up to date general, orthodontic and family dentistry. Our Clinic has grown to provide a world class facility for the treatment of tooth loss, dental cosmetics and advanced restorative dentistry.</p>
+                    <p>Our Dental Clinic is dedicated to providing the most up to date general, orthodontic and family dentistry. Our Clinic has grown to provide a world class facility for the treatment of tooth loss, dental cosmetics and advanced restorative dentistry.</p>
+                    </div>
+                </div>
+
+                <!-- Sample 02 -->
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 single-staff-panel">
+                    <div class="doctors-text-left">
+                    <p>Our Dental Clinic is dedicated to providing the most up to date general, orthodontic and family dentistry. Our Clinic has grown to provide a world class facility for the treatment of tooth loss, dental cosmetics and advanced restorative dentistry.</p>
+                    <p>Our Dental Clinic is dedicated to providing the most up to date general, orthodontic and family dentistry. Our Clinic has grown to provide a world class facility for the treatment of tooth loss, dental cosmetics and advanced restorative dentistry.</p>
+                    </div>
+                    <div class="doctors-text-doctor">
+                        <img src="assets/images/staff/staff-thumbnail.jpg" alt="" />
+                        <div class="doctors-text-doctor-name">Wyatt Esmond</div>
+                        <div class="doctors-text-doctor-position"> Chief of Dentists </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End: Main Section -->
+
+	<?php
+		// Including footer file
+		include("assets/php/globals/footer.php");
+	?>
+
+	<?php
+		// Including foot file
+		include("assets/php/globals/foot.php");
+	?>
+</body>
+</html>
