e891c60c54b06574894e3ca974c38a75ae44c9af
Srinivasa Prasad Ghantasala <prasad@bluplateau.com>
2018-03-28 01:49:37 +1300

    Adjusted UI & fixed the missing images

--- qcard.php
+++ qcard.php
@@ -12,7 +12,7 @@
         ?>
         
         <!-- Start: Main Section -->
-        <div class="page-title page-title-contacts bg-pattern" data-bgcolor="3B4B89">
+        <div class="page-title bg-pattern" data-bgcolor="3B4B89">
             <div class="page-title-overlay">
                 <div class="container">
                 <div class="row">
@@ -26,44 +26,42 @@
         </div>
     
         <!-- FAQ -->
-        <div class="tipsfaq">
-            <div class="container">
-                <div class="row">
-                <h2 class="section-title text-center">
-                    <span class="bold700">
-                        Finance Options
-                    </span>
-                </h2>
-                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                        <div class="panel-group" id="accordion"> 
-                            <!-- FAQ ITEM -->
-                            <div class="panel panel-default">
-                                <div class="panel-heading">
-                                <h4 class="panel-title"> <a data-parent="#accordion"> <i class="fa fa-cc-mastercard"></i> Q MasterCard® &amp; Q Card </a> </h4>
-                                </div>
-                                <div id="collapseOne" class="panel-collapse qcard">
-                                    <div class="panel-body">
-                                        <h2 class="section-title text-center">
-                                            <span class="bold700">
-                                                Q MasterCard® &amp; Q Card
-                                            </span>
-                                        </h2>
-                                        <a href="https://wss.qmastercard.co.nz/Osso/mcard/initialize?tid=6bS_pIaNEbwHgYbSCRFcjQ&amp;requestvalue=-N8i2s2SkQ4zKCIxRbVBTtM-oQ7yySVlb4ngMnA1cRdRxfjXFE2KWJ2S5lrt7xjO&murl=https://www.qmastercard.co.nz">
-                                            <img src="assets/images/qcard/qcard.png" class="qcard-img" alt="">
-                                        </a>
-                                        <p>
-                                            We offer multiple finance options with Q MasterCard and Q Card. You automatically get 3 months no payments and no interest#  with Q MasterCard or Q Card through the EFTPOS machine. For full priced treatments $300 and over, there's 12 months no payments and no interest. If you don't already have a card you can apply for a Q MasterCard online by clicking HERE. For more information visit qmastercard.co.nz or ask us. Lending criteria, fees, terms and conditions apply. Mastercard is a registered trademark of Mastercard International Incorporated.
-                                        </p>
+        <div class="container finance-options">
+            <div class="row">
+            <h2 class="section-title text-center">
+                <span class="bold700">
+                    Finance Options
+                </span>
+            </h2>
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div class="panel-group" id="accordion"> 
+                        <!-- FAQ ITEM -->
+                        <div class="panel panel-default">
+                            <div class="panel-heading">
+                            <h4 class="panel-title"> <a data-parent="#accordion"> <i class="fa fa-cc-mastercard"></i> Q MasterCard® &amp; Q Card </a> </h4>
+                            </div>
+                            <div id="collapseOne" class="panel-collapse qcard">
+                                <div class="panel-body">
+                                    <h2 class="section-title text-center">
+                                        <span class="bold700">
+                                            Q MasterCard® &amp; Q Card
+                                        </span>
+                                    </h2>
+                                    <a href="https://wss.qmastercard.co.nz/Osso/mcard/initialize?tid=6bS_pIaNEbwHgYbSCRFcjQ&amp;requestvalue=-N8i2s2SkQ4zKCIxRbVBTtM-oQ7yySVlb4ngMnA1cRdRxfjXFE2KWJ2S5lrt7xjO&murl=https://www.qmastercard.co.nz">
+                                        <img src="assets/images/qcard/qcard.png" class="qcard-img" alt="">
+                                    </a>
+                                    <p>
+                                        We offer multiple finance options with Q MasterCard and Q Card. You automatically get 3 months no payments and no interest#  with Q MasterCard or Q Card through the EFTPOS machine. For full priced treatments $300 and over, there's 12 months no payments and no interest. If you don't already have a card you can apply for a Q MasterCard online by clicking HERE. For more information visit qmastercard.co.nz or ask us. Lending criteria, fees, terms and conditions apply. Mastercard is a registered trademark of Mastercard International Incorporated.
+                                    </p>
 
-                                        <h6>
-                                            #No payments and no interest for 3 months ("Payment Holiday") is available for in-store purchases on Q Eftpos and Q Mastercard. Annual Account Fee of $50 applies. Standard Interest Rate, currently 25.99% p.a. applies to any outstanding balance at end of Payment Holiday. Lending criteria, fees, terms and conditions apply. Rate and fees correct as at date of publication, subject to change.
-                                        </h6>
+                                    <h6>
+                                        #No payments and no interest for 3 months ("Payment Holiday") is available for in-store purchases on Q Eftpos and Q Mastercard. Annual Account Fee of $50 applies. Standard Interest Rate, currently 25.99% p.a. applies to any outstanding balance at end of Payment Holiday. Lending criteria, fees, terms and conditions apply. Rate and fees correct as at date of publication, subject to change.
+                                    </h6>
 
-                                        <h6>
-                                            ^No payments and no interest for 12 months (“Payment Holiday”) is available on Flexi Payment Plans in-store for full priced treatments $300 & over. Annual Account Fee of $50 applies. New Cardholder fee - $55 Establishment. Existing Cardholder fee - $35 Advance. Standard Interest Rate, currently 25.99% p.a. applies to any outstanding balance at end of Payment Holiday. Lending criteria, fees, terms and conditions apply. Rate and fees correct as at date of publication, subject to change. Mastercard and the Mastercard Brand Mark are registered trademarks of Mastercard International Incorporated.
-                                        </h6>
-                                        </div>
-                                </div>
+                                    <h6>
+                                        ^No payments and no interest for 12 months (“Payment Holiday”) is available on Flexi Payment Plans in-store for full priced treatments $300 & over. Annual Account Fee of $50 applies. New Cardholder fee - $55 Establishment. Existing Cardholder fee - $35 Advance. Standard Interest Rate, currently 25.99% p.a. applies to any outstanding balance at end of Payment Holiday. Lending criteria, fees, terms and conditions apply. Rate and fees correct as at date of publication, subject to change. Mastercard and the Mastercard Brand Mark are registered trademarks of Mastercard International Incorporated.
+                                    </h6>
+                                    </div>
                             </div>
                         </div>
                     </div>
