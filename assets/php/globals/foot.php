2bef371a803ad9594ff8bd5bfd1d757524fe9cd3
Srinivasa Prasad Ghantasala <prasad@bluplateau.com>
2018-03-27 14:54:05 +1300

    Added foot.php

--- assets/php/globals/foot.php
+++ assets/php/globals/foot.php
@@ -1,1 +1,59 @@
-<!-- Foot HTML Content -->
+<!--
+    ===================
+        START: FOOT FILES   
+    ===================
+-->
+
+<!-- Start: Common Files --> 
+<script src="assets/js/jquery.min.js"></script> 
+<script src="assets/js/bootstrap.min.js"></script>
+<!-- End: Common Files -->
+
+<!-- SLIDER PRO --> 
+<script src="assets/js/jquery.sliderPro.min.js"></script> 
+
+<!-- LIGHTBOX --> 
+<script src="assets/js/jquery.fancybox.pack.js"></script> 
+
+<!-- CAROUSEL --> 
+<script src="assets/js/owl.carousel.js"></script> 
+
+<!-- STAR RATING --> 
+<script src="assets/js/jquery.barrating.min.js"></script> 
+
+<!-- ISOTOPE FILTER --> 
+<script src="assets/js/isotope.pkgd.min.js"></script> 
+
+<!-- SCROLLSPY --> 
+<script src="assets/js/scrollspy.js"></script> 
+
+<!-- DATEPICKER --> 
+<script src="assets/js/moment.js"></script> 
+<script src="assets/js/bootstrap-datetimepicker.min.js"></script> 
+
+<!-- JQUERY FORM --> 
+<script src="assets/js/jquery.form.js"></script> 
+
+<!-- FORM VALIDATOR --> 
+<script src="assets/js/jquery.form-validator.js"></script> 
+
+<!-- SELECT STYLING --> 
+<script src="assets/js/jquery.selectBox.js"></script> 
+
+<!-- MATCHHEIGHT --> 
+<script src="assets/js/jquery.matchHeight.js"></script> 
+
+<!-- IMAGES LOADED --> 
+<script src="assets/js/imagesloaded.pkgd.min.js"></script> 
+
+<!-- CUSTOM SCRIPT --> 
+<script src="assets/js/theme.js"></script>
+
+<!-- GOOGLE MAPS --> 
+<script src="http://maps.googleapis.com/maps/api/js?key=AIzaSyC1N87a_NHjocaepKKcovPAYTMUkJBr9pQ&amp;language=en&amp;sensor=true"></script>
+
+<!--
+    ===================
+        END: FOOT FILES   
+    ===================
+-->
