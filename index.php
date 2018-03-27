ade7bd774aaaa16207acc69ed69cea13ee3dca82
Srinivasa Prasad Ghantasala <prasad@bluplateau.com>
2018-03-28 05:50:47 +1300

    Updated Testimonials

--- index.php
+++ index.php
@@ -77,7 +77,7 @@
 		<div class="service-title">
 			<h2>Services</h2>
 		</div><br>
-		<h4>At Stoddard Dental Square, we offer amenities and</h4>
+		<h4>At The Dental Hub, we offer amenities and</h4>
 		<h4>services not easily available in Auckland city</h4>
 	</div>
   
@@ -156,6 +156,63 @@
 	===========
 	END Partners
 	===========
+-->
+
+<!-- 
+	=============
+	TESTIMONIALS
+	=============
+-->
+<div class="review">
+  <div class="container">
+    <div class="row">
+      <h2 class="section-title">
+        <span class="bold700">
+          TESTIMONIALS
+        </span>
+      </h2>
+      <div class="review-container">
+        <div class="owl-review" id="owl-review"> 
+          
+          <!-- REVIEW ITEM -->
+          <div class="review-item">
+            <div class="review-item-text">This was my first dental treatment in many years and Dr. Callahan made me feel relaxed and calm. It was completely painless and issue free. I thoroughly recommend both Dr. Callahan and Dental clinic. Excellent caring staff! </div>
+            <div class="review-item-name">Caetlin Madyson</div>
+            <div class="review-item-quote"><img src="assets/images/quote.png" alt="" /></div>
+          </div>
+          
+          <!-- REVIEW ITEM -->
+          <div class="review-item">
+            <div class="review-item-text">Excellent service and great care was given at The Dental Hub to me when I visited their clinic for root canal treatment. I was very impressed by the personal touch and professional behavior. Thanks a lot to Shivam and his team. I will recommend them to everyone.</div>
+            <div class="review-item-name">Jessica</div>
+            <div class="review-item-quote"><img src="assets/images/quote.png" alt="" /></div>
+          </div>
+          
+          <!-- REVIEW ITEM -->
+          <div class="review-item">
+            <div class="review-item-text">I have been to The Dental Hub with a bad toothache. As per Dr. Mauly's advice, I underwent, Root Canal Treatment. The best part has been the way the Dr Mauly have identified the cause and guided me accordingly. She was so nice and considerate that I am very much impressed by the treatment I received. Big thanks for all her help.</div>
+            <div class="review-item-name">Megan Jade</div>
+            <div class="review-item-quote"><img src="assets/images/quote.png" alt="" /></div>
+          </div>
+          
+          <!-- REVIEW ITEM -->
+          <div class="review-item">
+            <div class="review-item-text">The Dental Hub staff is very friendly and helpful especially Dr. Nikita, she did her best to educate me about the treatment I will be receiving. She was very professional takes good care of her patients. One thing that appeals me about The Dental Hub is they maintain high level of standards for hygiene in their clinic.</div>
+            <div class="review-item-name">Connor</div>
+            <div class="review-item-quote"><img src="assets/images/quote.png" alt="" /></div>
+          </div>
+        </div>
+        <!-- REVIEW BUTTONS -->
+        <div class="carousel-btn carousel-next" id="next-review"><i class="fa fa-angle-right"></i></div>
+        <div class="carousel-btn carousel-prev" id="prev-review"><i class="fa fa-angle-left"></i></div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- 
+	=================
+	END TESTIMONIALS
+	=================
 -->
 
     <!-- End: Main Section -->
