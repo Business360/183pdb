710949275464c427eb3a46e718caf4129c214d8d
Srinivasa Prasad Ghantasala <prasad@bluplateau.com>
2018-03-28 02:37:02 +1300

    Added blog_details.php page

---
+++ blog_details.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+	// Including head file
+	include("assets/php/globals/head.php");
+?>
+<body>
+
+	<?php
+		// Including header file
+		include("assets/php/globals/header.php");
+    ?>
+    
+    <!-- Start: Main Section -->
+    <div class="page-title page-title-blog bg-pattern" data-bgcolor="199FBE">
+        <div class="page-title-overlay">
+            <div class="container">
+            <div class="row">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-home"><a href="index.php"><i class="fa fa-home"></i></a></li>
+                    <li class="active">Blog Details</li>
+                </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="blog blog-2 blog-3">
+        <div class="container">
+            <div class="row">
+            <h2 class="section-title">
+                <span class="bold700">
+                    Blog Details
+                </span>
+            </h2>
+            <div class="blog-container col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <div class="blog-item">
+                <div class="blog-item-desc single-desc">
+                    <div class="blog-item-title">
+                    <h3><a href="#">The Routine Dental Exams</a></h3>
+                    </div>
+                    <div class="blog-item-author"><a href="#">by Dr. Kaeden Bernard</a></div>
+                    <div class="blog-item-text">
+                    <p>Tooth whitening (termed tooth bleaching when utilizing bleach), is either restoration of natural tooth shade or whitening beyond natural tooth shade, depending on the definition used.</p>
+                    <p>Restoration of the underlying, natural tooth shade is possible by simply removing surface (extrinsic) stains (e.g. from tea, coffee, red wine and tobacco) and calculus (tartar). This is achieved by having the teeth cleaned by a dental professional (commonly termed "scale and polish".</p>
+                    <div class="quote">
+                        <div class="quote-quote"><img src="assets/images/blog/quote.png" alt=""></div>
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore reprehenderit in  eu fugiat nulla pariatur ullamco laboris.
+                        <div class="quote-info">
+                        <span class="quote-name">
+                            Jeanette Evvie,
+                        </span>
+                        <span class="quote-position">
+                            Student
+                        </span>
+                        </div>
+                    </div>
+                    <h3>Experience H3</h3>
+                    <p>Our Dental Clinic is dedicated to providing the most up to date general, orthodontic and family dentistry. Our Clinic has grown to provide a world class facility for the treatment of tooth loss, dental cosmetics and advanced restorative dentistry.</p>
+                    <ul class="text-list">
+                        <li>Our Dental Clinic is dedicated to providing the most up to date general</li>
+                        <li>orthodontic and family dentistry. </li>
+                        <li>Our Clinic has grown to provide a world class facility for the treatment of tooth loss, dental cosmetics and advanced restorative dentistry.</li>
+                    </ul>
+                    <p>Our Dental Clinic is dedicated to providing the most up to date general, orthodontic and family dentistry. Our Clinic has grown to provide a world class facility for the treatment of tooth loss, dental cosmetics and advanced restorative dentistry.</p>
+                    <p class="selected">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum.</p>
+                    <p>Dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto vitae dicta sunt explicabo.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <p>Dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto vitae dicta sunt explicabo. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                    <p>Tooth whitening (termed tooth bleaching when utilizing bleach), is either restoration of natural tooth shade or whitening beyond natural tooth shade, depending on the definition used.</p>
+                    <p>Restoration of the underlying, natural tooth shade is possible by simply removing surface (extrinsic) stains (e.g. from tea, coffee, red wine and tobacco) and calculus (tartar).</p>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+    <!-- End: Main Section -->
+
+	<?php
+		// Including footer file
+		include("assets/php/globals/footer.php");
+	?>
+
+	<?php
+		// Including foot file
+		include("assets/php/globals/foot.php");
+	?>
+</body>
+</html>
