7a58c9b872988e18b560a4a7355dc49ea4fd987b
Srinivasa Prasad Ghantasala <prasad@bluplateau.com>
2018-03-28 02:07:01 +1300

    Added blog.php

---
+++ blog.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+	// Including head file
+	include("assets/php/globals/head.php");
+?>
+<body>
+
+	<?php
+		// Including header file
+		include("assets/php/globals/header.php");
+    ?>
+    
+    <!-- Start: Main Section -->
+    <div class="page-title page-title-blog bg-pattern" data-bgcolor="199FBE">
+        <div class="page-title-overlay">
+            <div class="container">
+            <div class="row">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-home"><a href="index.php"><i class="fa fa-home"></i></a></li>
+                    <li class="active">Blog</li>
+                </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="blog blog-2 blog-3">
+        <div class="container">
+            <div class="row">
+                <div class="blog-container col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div class="row">
+                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                            <div class="blog-item">
+                            <div class="blog-item-image">
+                                <div class="blog-item-info">
+                                    <div class="blog-item-date col-xlg-6 col-lg-6 col-sm-6 col-md-6 col-xs-12"><i class="fa fa-calendar-o"></i> 03.10.2016</div>
+                                    <div class="blog-item-category col-xlg-6 col-lg-6 col-sm-6 col-md-6 col-xs-12"><i class="fa fa-folder-o"></i> Teeth cleaning</div>
+                                </div>
+                            </div>
+                            <div class="blog-item-desc">
+                                <div class="blog-item-title">
+                                <h3><a href="#">Oral exams</a></h3>
+                                </div>
+                                <div class="blog-item-author"><a href="#">by Dr. Kaeden Bernard</a></div>
+                                <div class="blog-item-text">
+                                <p>Tooth whitening (termed tooth bleaching when utilizing bleach), is either restoration of natural tooth shade or whitening beyond natural tooth shade, depending on the definition used.</p>
+                                <p>Restoration of the underlying, natural tooth shade is possible by simply removing surface (extrinsic) stains (e.g. from tea, coffee, red wine and tobacco) and calculus (tartar). This is achieved by having the teeth cleaned by a dental professional (commonly termed "scale and polish".</p>
+                                </div>
+                                <div class="blog-item-button"><a href="#" class="btn btn-primary-1">READ MORE</a></div>
+                            </div>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                            <div class="blog-item">
+                            <div class="blog-item-image">
+                                <div class="blog-item-info">
+                                    <div class="blog-item-date col-xlg-6 col-lg-6 col-sm-6 col-md-6 col-xs-12"><i class="fa fa-calendar-o"></i> 03.10.2016</div>
+                                    <div class="blog-item-category col-xlg-6 col-lg-6 col-sm-6 col-md-6 col-xs-12"><i class="fa fa-folder-o"></i> Teeth cleaning</div>
+                                </div>
+                            </div>
+                            <div class="blog-item-desc">
+                                <div class="blog-item-title">
+                                <h3><a href="#">Oral exams</a></h3>
+                                </div>
+                                <div class="blog-item-author"><a href="#">by Dr. Kaeden Bernard</a></div>
+                                <div class="blog-item-text">
+                                <p>Tooth whitening (termed tooth bleaching when utilizing bleach), is either restoration of natural tooth shade or whitening beyond natural tooth shade, depending on the definition used.</p>
+                                <p>Restoration of the underlying, natural tooth shade is possible by simply removing surface (extrinsic) stains (e.g. from tea, coffee, red wine and tobacco) and calculus (tartar). This is achieved by having the teeth cleaned by a dental professional (commonly termed "scale and polish".</p>
+                                </div>
+                                <div class="blog-item-button"><a href="#" class="btn btn-primary-1">READ MORE</a></div>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                            <div class="blog-item">
+                            <div class="blog-item-image">
+                                <div class="blog-item-info">
+                                    <div class="blog-item-date col-xlg-6 col-lg-6 col-sm-6 col-md-6 col-xs-12"><i class="fa fa-calendar-o"></i> 03.10.2016</div>
+                                    <div class="blog-item-category col-xlg-6 col-lg-6 col-sm-6 col-md-6 col-xs-12"><i class="fa fa-folder-o"></i> Teeth cleaning</div>
+                                </div>
+                            </div>
+                            <div class="blog-item-desc">
+                                <div class="blog-item-title">
+                                <h3><a href="#">Oral exams</a></h3>
+                                </div>
+                                <div class="blog-item-author"><a href="#">by Dr. Kaeden Bernard</a></div>
+                                <div class="blog-item-text">
+                                <p>Tooth whitening (termed tooth bleaching when utilizing bleach), is either restoration of natural tooth shade or whitening beyond natural tooth shade, depending on the definition used.</p>
+                                <p>Restoration of the underlying, natural tooth shade is possible by simply removing surface (extrinsic) stains (e.g. from tea, coffee, red wine and tobacco) and calculus (tartar). This is achieved by having the teeth cleaned by a dental professional (commonly termed "scale and polish".</p>
+                                </div>
+                                <div class="blog-item-button"><a href="#" class="btn btn-primary-1">READ MORE</a></div>
+                            </div>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                            <div class="blog-item">
+                            <div class="blog-item-image">
+                                <div class="blog-item-info">
+                                    <div class="blog-item-date col-xlg-6 col-lg-6 col-sm-6 col-md-6 col-xs-12"><i class="fa fa-calendar-o"></i> 03.10.2016</div>
+                                    <div class="blog-item-category col-xlg-6 col-lg-6 col-sm-6 col-md-6 col-xs-12"><i class="fa fa-folder-o"></i> Teeth cleaning</div>
+                                </div>
+                            </div>
+                            <div class="blog-item-desc">
+                                <div class="blog-item-title">
+                                <h3><a href="#">Oral exams</a></h3>
+                                </div>
+                                <div class="blog-item-author"><a href="#">by Dr. Kaeden Bernard</a></div>
+                                <div class="blog-item-text">
+                                <p>Tooth whitening (termed tooth bleaching when utilizing bleach), is either restoration of natural tooth shade or whitening beyond natural tooth shade, depending on the definition used.</p>
+                                <p>Restoration of the underlying, natural tooth shade is possible by simply removing surface (extrinsic) stains (e.g. from tea, coffee, red wine and tobacco) and calculus (tartar). This is achieved by having the teeth cleaned by a dental professional (commonly termed "scale and polish".</p>
+                                </div>
+                                <div class="blog-item-button"><a href="#" class="btn btn-primary-1">READ MORE</a></div>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End: Main Section -->
+
+	<?php
+		// Including footer file
+		include("assets/php/globals/footer.php");
+	?>
+
+	<?php
+		// Including foot file
+		include("assets/php/globals/foot.php");
+	?>
+</body>
+</html>
