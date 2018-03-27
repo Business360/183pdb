3da8c967d953d1831bb776605dfa76b2bff6ade6
Srinivasa Prasad Ghantasala <prasad@bluplateau.com>
2018-03-28 03:17:14 +1300

    Added services.php page

---
+++ services.php
@@ -0,0 +1,205 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+	// Including head file
+	include("assets/php/globals/head.php");
+?>
+<body>
+
+	<?php
+		// Including header file
+		include("assets/php/globals/header.php");
+    ?>
+    
+    <!-- Start: Main Section -->
+    <div class="page-title page-title-services bg-pattern" data-bgcolor="2791AA">
+		<div class="page-title-overlay">
+			<div class="container">
+				<div class="row">
+					<ol class="breadcrumb">
+						<li class="breadcrumb-home"><a href="index.php"><i class="fa fa-home"></i></a></li>
+						<li class="active">Contact us</li>
+					</ol>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="services-1" id="services">
+		<div class="container">
+			<div class="row">
+				<h2 class="section-title text-center">
+					<span class="bold700">
+						We provide the full range of dental services
+					</span>
+				</h2>
+				<p>At Stoddard Dental Square, we strive to make experience of our patient personalized and comfortable every time. Our clinic unites advanced dentistry technology with a preventative philosophy to accomplish not only healthy teeth and gums but confident, beautiful smiles. Stoddard Dental Square work with a philosophy that “Best experience is the most valuable thing that we can offer to our patients” so Dentist and support staff of Stoddard Dental Square try their level best to give best experience to each and every patient.</p>
+			</div>
+		</div>
+	</div>
+
+	<div class="serv">
+		<ul id="isotope-filter">
+			<li><a class="active" data-filter="*">All services</a></li>
+			<li><a data-filter=".general_dentistry">General Dentistry</a></li>
+			<li><a data-filter=".emergency_dental_treatment">Emergency Dental Treatment</a></li>
+			<li><a data-filter=".affordable_cosmetic_services">Affordable Cosmetic Services</a></li>
+			<li><a data-filter=".smile_makeovers">Smile Makeovers</a></li>
+		</ul>
+		
+		<div class="container">
+			<div class="row">
+				<div class="serv-items" id="isotope-items">
+					<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 serv-item isotope-item general_dentistry">
+						<div class="serv-item-text">
+							<h3>Pain-free Treatment</h3>
+							<p>We understand that dental treatment is a painful experience but at Stoddard Dental Square we always make sure that you our patients always get pain-free treatments after they get numb. They feel slight pinch of pain when we give an injection at the starting of treatment and assure you that the pinch also go within a minute.</p>
+							<div class="serv-item-tag">General Dentistry</div>
+						</div>
+					</div>
+
+						<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 serv-item isotope-item general_dentistry">
+							<div class="serv-item-text">
+								<h3>Routine Dental Exam</h3>
+								<p>The routine dental test about polishing and cleaning include x-rays test (taken once a year) is done just to make sure that there is no decays in between teeth. Scaling session make sure that your teeth are cleaned. All the deposits and debris are cleaned away from everywhere even in between your teeth and teeth are polished to give you a clean and even surface. It is recommended to get routine dental check up after every 6 months or a year for oral hygiene.</p>
+								<div class="serv-item-tag">General Dentistry</div>
+							</div>
+						</div>
+
+						<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 serv-item isotope-item general_dentistry">
+							<div class="serv-item-text">
+								<h3>Scaling and Polishing</h3>
+								<p>Scaling is a process of getting your teeth cleaned using ultrasonic scalar which applies high vibrations and water to take out that unwanted deposits on your teeth. Polishing is done after scaling to make sure that no sharp points anywhere and your teeth is left behind.</p>
+								<div class="serv-item-tag">General Dentistry</div>
+							</div>
+						</div>
+
+						<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 serv-item isotope-item general_dentistry">
+							<div class="serv-item-text">
+								<h3>Periodontal (Gum) Treatment</h3>
+								<p>Bleeding, sore gums, bad breath and loose teeth are something’s that you shouldn’t be living with and these are the few indications of periodontal sickness. After giving local anesthesia, proper cleaning is done at gum level at a time interval of 15 days. Patient has to keep up with the regular scale, check up, and polish and also maintain your oral hygiene routine at home.</p>
+								<div class="serv-item-tag">General Dentistry</div>
+							</div>
+						</div>
+
+						<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 serv-item isotope-item general_dentistry">
+							<div class="serv-item-text">
+								<h3>Tooth Colour Fillings</h3>
+								<p>At times we often feel discomfort and pain when food stuck between our teeth due tooth decay or cavity. Situation like this can be easily fixed by filling and restoring the teeth. The basic idea behind a filling is to take out all the decayed part of your tooth and then fill it up. And now we have materials that are strong enough and can have the same shade as your natural teeth which will help you have that perfect smile as well as get rid of that decay. Patient can eat or drink immediately after the filling is done as the material will be set and shaped before you leave the clinic.</p>
+								<div class="serv-item-tag">General Dentistry</div>
+							</div>
+						</div>
+
+						<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 serv-item isotope-item general_dentistry">
+							<div class="serv-item-text">
+								<h3>Root Canal Treatment</h3>
+								<p>Human teeth’s have canals in the center which act as connection for nerves and blood vessels. A deep decay, infection or any type of accident can cause harm to these blood vessels and cause you a lot of pain. A root canal treatment can help to treat this condition. In this scenario first we open up the canals, take out the septic blood vessels and nerves, clean up the root canal and at last fill it up with a filling material in-order to avoid any future infection. The whole treatment is done in span of 2 to 3 sittings.</p>
+								<div class="serv-item-tag">General Dentistry</div>
+							</div>
+						</div>
+
+						<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 serv-item isotope-item general_dentistry">
+							<div class="serv-item-text">
+								<h3>Tooth Extraction</h3>
+								<p>If a tooth has been damaged by decay or broken by accident, at first instances we try to restore it with crown and filling. At times, damage is too much for the tooth to be repaired. In such a scenario, we try to make this ordeal as painless. We make sure that you are numb before starting any procedure and during the whole treatment time you are free to stop us at any time if you are having any pain or discomfort. We remove the teeth from its socket in the bone with precise and care. If you follow the pedagogies of Stoddard dental square, you will not face any complications after tooth extraction in future.</p>
+								<div class="serv-item-tag">General Dentistry</div>
+							</div>
+						</div>
+
+						<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 serv-item isotope-item general_dentistry">
+							<div class="serv-item-text">
+								<h3>Dentures</h3>
+								<p>Dentures are set of removable, substitute for missing teeth and surrounding tissues. They not only help you gain back your confidence but also enhance your ability to chew by replacing edentulous areas with denture teeth. As per your need, you can either have a partial denture to replace only the missing teeth or a full denture if you have lost all your teeth. Dentures are also available in various type of materials to help you get adjusted.</p>
+								<div class="serv-item-tag">General Dentistry</div>
+							</div>
+						</div>
+
+						<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 serv-item isotope-item general_dentistry">
+							<div class="serv-item-text">
+								<h3>Veneers</h3>
+								<p>It is a layer of composite or dental porcelain material placed over a teeth, either to improve the esthetics of a teeth or to defend the tooth’s surface from impairment. They act as a shell that cover the external layer of your teeth and come in a variety of color shades which can be chosen to exactly match the color of your teeth.</p>
+								<div class="serv-item-tag">General Dentistry</div>
+							</div>
+						</div>
+
+						<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 serv-item isotope-item general_dentistry">
+							<div class="serv-item-text">
+								<h3>Dental Crowns</h3>
+								<p>It is an artificial restitution that fits over the remaining part of prepared teeth, making it strong and giving it the shape of natural teeth. A crown is sometimes known as a ‘cap’. Unlike removable devices such as dentures, which are removable and clean daily, crowns are permanently fixed onto existing teeth or implants, and can only be removed by a dentist. Crown implantation requires two sitting with a gap of 15 days.</p>
+								<div class="serv-item-tag">General Dentistry</div>
+							</div>
+						</div>
+
+						<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 serv-item isotope-item general_dentistry">
+							<div class="serv-item-text">
+								<h3>Dental Bridges</h3>
+								<p>IBridges are primarily used to replace one or more missing teeth. A bridge is made up of two or more than two crowns. Bridges are cemented to the natural teeth or implants surrounding the empty space. These teeth, known as abutments, act as anchors for the bridge. The procedure timing is similar to that for a dental crown.</p>
+								<div class="serv-item-tag">General Dentistry</div>
+							</div>
+						</div>
+
+						<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 serv-item isotope-item general_dentistry">
+							<div class="serv-item-text">
+								<h3>Free WINZ Quotes</h3>
+								<p>Stoddard Dental Square provide free consultation and treatment for Work and Income NZ quote so that you can get your teeth treated without worrying about how you are going to pay for your Emergency dental treatment.</p>
+								<div class="serv-item-tag">General Dentistry</div>
+							</div>
+						</div>
+
+						<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 serv-item isotope-item general_dentistry">
+							<div class="serv-item-text">
+								<h3>Free treatment for under 18</h3>
+								<p>Stoddard Dental Square provide free treatment for teenagers of the age group of 13 to 18. Children below 13 year of age either get a referral letter from their school dentist or they have to pay for the treatment.</p>
+								<div class="serv-item-tag">General Dentistry</div>
+							</div>
+						</div>
+
+						<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 serv-item isotope-item general_dentistry">
+							<div class="serv-item-text">
+								<h3>Free ACC Treatment</h3>
+								<p>We are certified dentistry service provider of Accident Compensation Commission and we make sure that you get the care, you deserve after the terrible accident.</p>
+								<div class="serv-item-tag">General Dentistry</div>
+							</div>
+						</div>
+
+						<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 serv-item isotope-item general_dentistry">
+							<div class="serv-item-text">
+								<h3>Teeth Whitening</h3>
+								<p>Teeth whitening process lighten up color of your teeth and help to get rid of discoloration and stains. This need to be repeated at regular interval to maintain the brighter color of your teeth. We also offer take home kits which can help you whiten your teeth at your comfort station.</p>
+								<div class="serv-item-tag">General Dentistry</div>
+							</div>
+						</div>
+
+						<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 serv-item isotope-item general_dentistry">
+							<div class="serv-item-text">
+								<h3>Mouth Guards</h3>
+								<p>A mouth guard is a protective cushion that covers the gums and teeth to prevent and reduce the chances of injury to the gums, arches, teeth and lips. If you are a sports freak and you require a mouth guard to protect your teeth, we can make a customize mouth guard that fits you and let you concentrate on your game.</p>
+								<div class="serv-item-tag">General Dentistry</div>
+							</div>
+						</div>
+
+						<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 serv-item isotope-item general_dentistry">
+							<div class="serv-item-text">
+								<h3>Gold Teeth</h3>
+								<p>Do you want a golden smile on your face that can gain attention of everyone around you? We can help you achieve the same in just two sittings. We can put a gold crowns which will cover the whole of your teeth. This is a good option for those who have the habit of grinding teeth at night as this is one of the strongest crowns available in the market till date.</p>
+								<div class="serv-item-tag">General Dentistry</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+    <!-- End: Main Section -->
+
+	<?php
+		// Including footer file
+		include("assets/php/globals/footer.php");
+	?>
+
+	<?php
+		// Including foot file
+		include("assets/php/globals/foot.php");
+	?>
+</body>
+</html>
