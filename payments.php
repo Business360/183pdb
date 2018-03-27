2026e1fa0224819882ae6b620bf0c7dc51d253be
Srinivasa Prasad Ghantasala <prasad@bluplateau.com>
2018-03-28 00:37:54 +1300

    Added Payments.php page

---
+++ payments.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+	// Including head file
+	include("assets/php/globals/head.php");
+?>
+<body>
+
+	<?php
+		// Including header file
+		include("assets/php/globals/header.php");
+    ?>
+    
+    <!-- Start: Main Section -->
+    <div class="page-title page-title-contacts bg-pattern" data-bgcolor="3B4B89">
+        <div class="page-title-overlay">
+            <div class="container">
+            <div class="row">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-home"><a href="index.php"><i class="fa fa-home"></i></a></li>
+                    <li class="active">Payment Options</li>
+                </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+   
+    <!-- FAQ -->
+    <div class="tipsfaq">
+        <div class="container">
+            <div class="row">
+            <h2 class="section-title text-center">
+                <span class="bold700">
+                    Payment Options
+                </span>
+            </h2>
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div class="panel-group" id="accordion"> 
+                    <!-- FAQ ITEM -->
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                        <h4 class="panel-title"> <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne"> <i class="fa fa-heartbeat"></i> Surgical implant placements </a> </h4>
+                        </div>
+                        <div id="collapseOne" class="panel-collapse collapse in">
+                        <div class="panel-body"> Dental implant placement is no longer performed only by oral surgeons and periodontists; general dentists are also increasingly providing difficult surgical implant services. Dental implants may be used to replace single teeth, replace multiple teeth, or provide abutments for complete dentures or partials. This topic focuses on the placement of single-tooth dental implants. </div>
+                        </div>
+                    </div>
+                    
+                    <!-- FAQ ITEM -->
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                        <h4 class="panel-title"> <a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" class="collapsed"> <i class="fa fa-medkit"></i> Teeth whitening </a> </h4>
+                        </div>
+                        <div id="collapseTwo" class="panel-collapse collapse">
+                        <div class="panel-body"> Dental implant placement is no longer performed only by oral surgeons and periodontists; general dentists are also increasingly providing difficult surgical implant services. Dental implants may be used to replace single teeth, replace multiple teeth, or provide abutments for complete dentures or partials. This topic focuses on the placement of single-tooth dental implants. </div>
+                        </div>
+                    </div>
+                    
+                    <!-- FAQ ITEM -->
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                        <h4 class="panel-title"> <a data-toggle="collapse" data-parent="#accordion" href="#collapseThree" class="collapsed"> <i class="fa fa-life-bouy"></i> Teeth cleaning </a> </h4>
+                        </div>
+                        <div id="collapseThree" class="panel-collapse collapse">
+                        <div class="panel-body"> Dental implant placement is no longer performed only by oral surgeons and periodontists; general dentists are also increasingly providing difficult surgical implant services. Dental implants may be used to replace single teeth, replace multiple teeth, or provide abutments for complete dentures or partials. This topic focuses on the placement of single-tooth dental implants. </div>
+                        </div>
+                    </div>
+                    
+                    <!-- FAQ ITEM -->
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                        <h4 class="panel-title"> <a data-toggle="collapse" data-parent="#accordion" href="#collapseFour" class="collapsed"> <i class="fa fa-flask"></i> X-rays </a> </h4>
+                        </div>
+                        <div id="collapseFour" class="panel-collapse collapse">
+                        <div class="panel-body"> Dental implant placement is no longer performed only by oral surgeons and periodontists; general dentists are also increasingly providing difficult surgical implant services. Dental implants may be used to replace single teeth, replace multiple teeth, or provide abutments for complete dentures or partials. This topic focuses on the placement of single-tooth dental implants. </div>
+                        </div>
+                    </div>
+                    
+                    <!-- FAQ ITEM -->
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                        <h4 class="panel-title"> <a data-toggle="collapse" data-parent="#accordion" href="#collapseFive" class="collapsed"> <i class="fa fa-ambulance"></i> Crowns </a> </h4>
+                        </div>
+                        <div id="collapseFive" class="panel-collapse collapse">
+                        <div class="panel-body"> Dental implant placement is no longer performed only by oral surgeons and periodontists; general dentists are also increasingly providing difficult surgical implant services. Dental implants may be used to replace single teeth, replace multiple teeth, or provide abutments for complete dentures or partials. This topic focuses on the placement of single-tooth dental implants. </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End: Main Section -->
+
+	<?php
+		// Including footer file
+		include("assets/php/globals/footer.php");
+	?>
+
+	<?php
+		// Including foot file
+		include("assets/php/globals/foot.php");
+	?>
+</body>
+</html>
