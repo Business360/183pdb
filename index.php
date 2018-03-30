8ec06b5a671651cf938c7c7bbf4a833472f5fc63
Srinivasa Prasad Ghantasala <prasad@bluplateau.com>
2018-03-31 01:40:03 +1300

    Updated Slider images

--- index.php
+++ index.php
@@ -22,7 +22,35 @@
 	<div class="sp-slides"> 
 	
 		<!-- HEADER SLIDER ITEM -->
-		<div class="sp-slide"> <img class="sp-image" src="assets/images/blank.gif" alt="" data-src="#" />
+		<div class="sp-slide">
+			<img class="sp-image" alt="" data-src="assets/images/slider/slide_01.jpg" />
+			<div class="container">
+				<h2 class="sp-layer slider-subtitle" 
+									data-position="leftCenter" data-horizontal="15" data-vertical="-120" 
+									data-show-transition="left" data-hide-transition="left"
+									data-show-delay="400" data-hide-delay="400"> <b>FREE WINZ QUOTE</b> </h2>
+				<h2 class="sp-layer slider-subtitle" 
+									data-position="leftCenter" data-horizontal="15" data-vertical="-120" 
+									data-show-transition="left" data-hide-transition="left"
+									data-show-delay="400" data-hide-delay="400"> <b>FREE ACC TREATMENTS</b> </h2>
+				<h2 class="sp-layer slider-subtitle" 
+									data-position="leftCenter" data-horizontal="15" data-vertical="-120" 
+									data-show-transition="left" data-hide-transition="left"
+									data-show-delay="400" data-hide-delay="400"> <b>Free INITIAL CONSULTATION</b> </h2>
+				<h2 class="sp-layer slider-subtitle" 
+									data-position="leftCenter" data-horizontal="15" data-vertical="-120" 
+									data-show-transition="left" data-hide-transition="left"
+									data-show-delay="400" data-hide-delay="400"> <b>FREE TREATMENT UNDER 18</b> </h2> <br><br><br>
+				<h1 class="sp-layer slider-title" 
+									data-position="leftCenter" data-horizontal="15" data-vertical="0" 
+									data-show-transition="left" data-hide-transition="left"
+									data-show-delay="200" data-hide-delay="200">OPEN 7 DAYS</h1>
+			</div>
+		</div>
+	
+		<!-- HEADER SLIDER ITEM -->
+		<div class="sp-slide">
+			<img class="sp-image" alt="" data-src="assets/images/slider/slide_02.jpg" />
 			<div class="container">
 				<h2 class="sp-layer slider-subtitle" 
 									data-position="leftCenter" data-horizontal="15" data-vertical="-120" 
