b96209196ac5dc0f246e0d6f942206cdc7bc952b
Srinivasa Prasad Ghantasala <prasad@bluplateau.com>
2018-04-05 22:55:46 +1200

    Bug fix for responsive header design

--- assets/php/globals/header.php
+++ assets/php/globals/header.php
@@ -1,57 +1,18 @@
 <!-- Header HTML Content -->
 
 <!-- =========================
-     TOP BAR
-============================== -->
-<div class="top-bar" id="top-bar">
-  <div class="container">
-    <div class="row">
-		<div class="col-lg-7 col-md-7 col-sm-7 col-xs-12 col-lg-offset-5 col-md-offset-5 col-sm-offset-5">
-			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="margin-left: 105px;">
-				<div class="top-bar-adress">
-					<i class="flaticon-navigation-arrow"></i> <b>Address:</b> <a href="https://goo.gl/maps/VsSM4uHKyGS2" target="_blank">73A Rosebank Road, Behind Subway - Avondale , Auckland 1026</a>
-				</div>
-				<div class="top-bar-mail">
-					<a href="#">
-						<i class="fa fa-phone"></i>
-					</a>
-					<a href="tel:0800 349 86684">0800 FIX TOOTH (0800 349 86684)</a> | 
-					<a href="#">
-						<i class="fa fa-phone"></i>
-					</a>
-					<a href="tel:092187777">09 218 7777</a>
-				</div>
-				<div class="top-bar-social">
-					<a href="#">
-						<i class="fa fa-envelope"></i>
-					</a>
-					<a href="#">info@thedentalhub.co.nz</a>
-					<a href="https://www.facebook.com/thedentalhubavondale" target="_blank">
-						<i class="fa fa-facebook"></i>
-					</a>
-					<a href="https://goo.gl/maps/VsSM4uHKyGS2" target="_blank">
-						<i class="fa fa-google"></i>
-					</a>
-				</div>
-				<a href="appointments.php" class="fancybox-2 btn btn-default" id="header-make-an-appointment-spacing">
-					<span class="plus">+</span> MAKE AN APPOINTMENT
-				</a>
-			</div>
-		</div>
-    </div>
-  </div>
-</div>
-<!-- =========================
-     END TOP BAR   
-============================== -->
-
-
-<!-- =========================
      HEADER
 ============================== -->
 <div class="header" id="header">
+
+	<!-- Header Left -->
 	<div class="container">
-		<div class="row"> 
+		<div class="row">
+
+			<div class="mobile-bar">
+				<div class="show-menu" id="show-menu"> <i class="fa fa-bars"></i> </div>
+				<div class="logo-for-mobile"> <a href="#" title="#"> <img src="assets/images/logo.png" alt=""> </a> </div>
+			</div>
 
 		<!-- HEADER LOGO -->
 			<div class="col-lg-3 col-md-3 col-sm-4 col-xs-12">
@@ -63,9 +24,62 @@
 			</div>
 		</div>
 	</div>
+	<!-- End: Header Left -->
+	
+	<!-- Header Right -->
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-7 col-md-7 col-sm-7 col-xs-12 col-lg-offset-5 col-md-offset-5 col-sm-offset-5 top-bar">
+				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 col-lg-offset-2 col-md-offset-2 col-sm-offset-2">
+					<div class="top-bar-adress">
+						<i class="flaticon-navigation-arrow"></i> <b>Address:</b> <a href="https://goo.gl/maps/VsSM4uHKyGS2" target="_blank">73A Rosebank Road, Behind Subway - Avondale , Auckland 1026</a>
+					</div>
+					<div class="top-bar-mail hidden-xs">
+						<a href="#">
+							<i class="fa fa-phone"></i>
+						</a>
+						<a href="tel:0800 349 86684">0800 FIX TOOTH (0800 349 86684)</a> | 
+						<a href="#">
+							<i class="fa fa-phone"></i>
+						</a>
+						<a href="tel:092187777">09 218 7777</a>
+					</div>
+					<div class="top-bar-social hidden-xs">
+						<a href="#">
+							<i class="fa fa-envelope"></i>
+						</a>
+						<a href="#">info@thedentalhub.co.nz</a>
+						<a href="https://www.facebook.com/thedentalhubavondale" target="_blank">
+							<i class="fa fa-facebook"></i>
+						</a>
+						<a href="https://goo.gl/maps/VsSM4uHKyGS2" target="_blank">
+							<i class="fa fa-google"></i>
+						</a>
+					</div>
+					<a href="appointments.php" class="fancybox-2 btn btn-default hidden-xs" id="header-make-an-appointment-spacing">
+						<span class="plus">+</span> MAKE AN APPOINTMENT
+					</a>
+				</div>
+				<a href="appointments.php" class="fancybox-2 btn btn-default hidden-sm hidden-md hidden-lg hidden-xlg" id="header-make-an-appointment-spacing">
+						<span class="plus">+</span> CALL US NOW
+					</a>
+			</div>
+		</div>
+	  </div>
+	<!-- End: Header Right -->
 </div>
 <!-- =========================
      END HEADER
+============================== -->
+
+<!-- =========================
+     TOP BAR
+============================== -->
+<div class="top-bar hidden-xs" id="top-bar">
+  
+</div>
+<!-- =========================
+     END TOP BAR   
 ============================== -->
 
 <!-- =========================
@@ -94,10 +108,6 @@
                 <div class="header-phone"> <i class="fa fa-phone"></i> 0800 FIX TOOTH (0800 349 86684) </div>
               </div>
 		  	</div>
-          <div class="mobile-bar">
-            <div class="show-menu" id="show-menu"> <i class="fa fa-bars"></i> </div>
-            <div class="logo-for-mobile"> <a href="#" title="#"> <img src="assets/images/logo.png" alt=""> </a> </div>
-          </div>
         </div>
         <ul class="nav navbar-nav">
           <li>
