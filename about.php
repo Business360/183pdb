90b971c89959e50f2f741b32e65ecaddcf70d144
Srinivasa Prasad Ghantasala <prasad@bluplateau.com>
2018-04-06 20:53:03 +1200

    Added "CALL" to Mobile UI

--- about.php
+++ about.php
@@ -24,6 +24,20 @@
             </div>
         </div>
     </div>
+
+    <!--
+        ====================
+        START: MOBILE DESIGN
+        ====================
+    -->
+        <a href="appointments.php" class="fancybox-2 btn btn-default hidden-sm hidden-md hidden-lg hidden-xlg" id="header-call-button-spacing">
+            CALL
+        </a>
+    <!--
+        ==================
+        END: MOBILE DESIGN
+        ==================
+    -->
     
     <div class="history">
         <div class="container">
