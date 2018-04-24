bd75e4a1a4bc6603f7fcd72574798cba2f3d70ea
admin-PC\admin <achyuth.thatha@bluplateau.com>
2018-04-24 19:49:40 +0530

    search functionality updated.

--- search.php
+++ search.php
@@ -10,14 +10,22 @@
 		// Including header file
 		include("assets/php/globals/header.php");
 	?>
-
-	<?php
+	<div style="padding:20px 20px 20px 100px;">
+	<?php 
 		$searchstring =  $_GET['q'];
-		$searchdata = get_search_details($searchstring);
-		echo $searchdata;
-		//echo $_GET['page'];
 	?>
-
+	<h3>Search for the word : <?php echo $searchstring ?></h3><br>
+	<?php
+		$searchdata = get_search_details($searchstring);
+		//print_r($searchdata);
+		$i=1;
+		foreach($searchdata as $value){
+			$pagename = $value['page_title'];
+			echo "<a href='index.php?page=$pagename'><h4>".$i.". ".$value['page_title']."</h4></a>";
+			$i++;
+		}
+	?>
+</div>
 	<?php
 		// Including footer file
 		include("assets/php/globals/footer.php");
