bd2786266b4b19aafe2bef8916a36898e93c192a
Achyuth Kumar <achyuth.thatha@bluplateau.com>
2018-10-08 15:21:19 +0530

    Changes reversed.

--- assets/php/globals/footer.php
+++ assets/php/globals/footer.php
@@ -49,11 +49,7 @@
 						<div class="footer-item-3-phone"><i class="fa fa-phone"></i> 0800 FIX TOOTH (0800 349 86684) </div>
 						<div class="footer-item-3-location"><a href="https://goo.gl/maps/VsSM4uHKyGS2" target="_blank">73A Rosebank Road, Behind Subway - Avondale , Auckland 1026</a></div>
 						<div class="footer-item-3-mail"><i class="fa fa-envelope"></i> <a href="#">info@thedentalhub.co.nz</a> </div>
-						<div class="footer-item-3-socials"><a href="http://www.facebook.com/thedentalhubauckland"><i class="fa fa-facebook"></i></a> 
-							<a href="https://goo.gl/maps/VsSM4uHKyGS2"><i class="fa fa-google"></i></a>
-							<a href="#"><i class="fa fa-instagram"></i></a>
-							<a href="#"><i class="fa fa-linkedin"></i></a>
-						</div>
+						<div class="footer-item-3-socials"><a href="http://www.facebook.com/thedentalhubauckland"><i class="fa fa-facebook"></i></a> <a href="https://goo.gl/maps/VsSM4uHKyGS2"><i class="fa fa-google"></i></a></div>
 					</div>
 					<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 footer-copyright">
 						<div> Designed by <a href="http://www.bluplateau.co.nz/" target="_blank">Blu Plateau Limited</a> </div>
