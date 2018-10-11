594618f15d8ea383120516820c0b0e7ea58b7d41
Achyuth Kumar <achyuth.thatha@bluplateau.com>
2018-10-11 10:57:47 +0530

    link changed

--- assets/php/globals/header.php
+++ assets/php/globals/header.php
@@ -37,7 +37,7 @@
             <li class="active"><a href="index.php">Home</a></li>
             <li><a href="about.php">About</a></li>
             <li><a href="services.php">Services</a></li>
-            <li><a href="contact_us.php">Contact Us</a></li>
+            <li><a href="contact.php">Contact Us</a></li>
             <li><a href="appointments.php">Make An Appointment</a></li>
             
           </ul>
