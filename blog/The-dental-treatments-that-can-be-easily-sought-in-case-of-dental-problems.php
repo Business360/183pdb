f83645a0e8c210f8c54505e6b89a73cb57914bd7
admin-PC\admin <achyuth.thatha@bluplateau.com>
2018-10-19 09:21:44 +0530

    google seo files added

---
+++ blog/The-dental-treatments-that-can-be-easily-sought-in-case-of-dental-problems.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+	// Including head file
+	include("../assets/php/globals/blog_head.php");
+?>
+<body>
+
+	<?php
+		// Including header file
+		include("../assets/php/globals/blog_header.php");
+    ?>
+    
+    <!-- Start: Main Section -->
+    <div class="page-title page-title-blog bg-pattern" data-bgcolor="199FBE">
+        <div class="page-title-overlay">
+            <div class="container">
+            <div class="row">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-home"><a href="../index.php"><i class="fa fa-home"></i></a></li>
+                    <li class="active">Blog Details</li>
+                </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+
+	<!--
+        ====================
+        START: MOBILE DESIGN
+        ====================
+    -->
+	<a href="tel:080034986684" class="fancybox-2 btn btn-default hidden-sm hidden-md hidden-lg hidden-xlg" id="header-call-button-spacing">
+            CALL US
+        </a>
+    <!--
+        ==================
+        END: MOBILE DESIGN
+        ==================
+    -->
+<title>The Dental Treatments That Can Be Easily Sought in Case of Dental Problems</title>
+    <div class="blog blog-2 blog-3">
+        <div class="container">
+            <div class="row">
+            <h2 class="section-title">
+                <span class="bold700">
+                    Blog Details
+                </span>
+            </h2>
+            <div class="blog-container col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <div class="blog-item">
+                <div class="blog-item-desc single-desc">
+                    <div class="blog-item-title">
+                    <h3><a href="#">The Dental Treatments That Can Be Easily Sought in Case of Dental Problems</a></h3>
+                    </div>
+                    <div class="blog-item-text">
+                    <p>A visit to a dentist might seem like a daunting affair and many people are not very eager to visit dentist to get their dental health checked but just as the physical health is to be checked on a regular basis similarly dental health should also be monitored and professionally evaluated by a dentist so that less problems occur in future. Most people are well aware about the different kinds of good dental practices necessary for maintaining the oral health but it is important to put those known rules into practice so that dental problems can be curbed.</p>
+
+<p><b>The different kinds of dental problems that can occur in a lifetime</b></p>
+
+<p>Mostly poor oral hygiene is attributed as the main reasons for dental decay but this is not always true because due to several other reasons misalignment of teeth can occur. Birth defects can be present or the gum health can be poor. The most common dental problems which are faced by individuals include the following:</p>
+ 
+<p><b>Cavities</b>: Cavities appears as a blackish stain on the crown or root of a tooth and with the passage of time the black spot becomes enlarged and the tooth slowly loses its shape and strength. Such cavities can be curbed by practicing good dental hygiene and can also be treated effectively by <a href="http://www.thedentalhub.co.nz/">Dentists in Avondale Auckland City</a>. The dentists will be able to provide professional cleaning that will cleanse the teeth and sanitize the whole moth thereby eliminating the decay casing bacteria. However if the tooth is completely destroyed then extraction might be the only route available for protecting the gum and the adjoining teeth.</p>
+
+<p><b>Misaligned teeth</b>: Alignment issues can be present from birth or might have been caused by a wrong bite or injury. <a href="http://thedentalhub.co.nz/services.php">Cosmetic Dentistry in Rosebank Road</a> is adept at handling cosmetic issues that have been caused by injuries to the jaws or gums.In order to realign the teeth a form of invisible brace can be used.These braces are not made of metals and are transparent in color. It will adjust the wrong alignment of the teeth without marring the beautiful smile of the individual.</p>
+
+<p><b>Weakened or paled tooth</b>: This problem can occur if the surface of the tooth has become thin. Due to wear and tear the teeth can become weakened at the surface level. This problem can be solved by the use of veneers. The veneers are tough layers that are custom made to cover the weakened tooth. The color and shape of the veneer is completely matched with the original tooth of the person. </p>
+
+</p>Hence getting the oral health examined by a professional doctor is very important so that dental problems are treated on time.</p>
+
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+    <!-- End: Main Section -->
+
+	<?php
+		// Including footer file
+		include("../assets/php/globals/blog_footer.php");
+	?>
+
+	<?php
+		// Including foot file
+		include("../assets/php/globals/blog_foot.php");
+	?>
+</body>
+</html>
