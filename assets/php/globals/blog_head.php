6c751e495b08c1487b1b32d32f679ba6ad6dec19
admin-PC\admin <achyuth.thatha@bluplateau.com>
2018-10-19 09:28:21 +0530

    blog pages supporting files added.

---
+++ assets/php/globals/blog_head.php
@@ -0,0 +1,41 @@
+<?php
+
+// Including "modules lists"
+include("../assets/php/modules/modules_list.php");
+
+// Including configuration file
+include("../assets/php/config.php");
+
+?>
+
+<!--
+    ===================
+        START: HEAD FILES   
+    ===================
+-->
+
+<!-- Start: Meta Tags -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- End: Meta Tags -->
+
+<!-- Start: Common Files -->
+<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800,800italic,700italic,600italic,400italic,300italic" />
+<link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.css" />
+<link rel="stylesheet" type="text/css" href="../assets/css/theme.css" />
+<link rel="stylesheet" type="text/css" href="../assets/css/responsive.css" />
+<link rel="stylesheet" type="text/css" href="../assets/css/jquery.fancybox.css" />
+<link rel="stylesheet" type="text/css" href="../assets/css/owl.carousel.css" />
+<link rel="stylesheet" type="text/css" href="../assets/css/owl.theme.css" />
+<link rel="stylesheet" type="text/css" href="../assets/css/owl.transitions.css" />
+<link rel="stylesheet" type="text/css" href="../assets/css/bootstrap-datetimepicker.css" />
+<link rel="stylesheet" type="text/css" href="../assets/css/jquery.selectBox.css" />
+<link rel="stylesheet" type="text/css" href="../assets/css/custom.css" />
+<link rel="stylesheet" type="text/css" href="../assets/css/plugins/flaticons/flaticon.css" />
+<link rel="stylesheet" type="text/css" href="../assets/css/plugins/font-awesome/css/font-awesome.min.css" />
+<!-- End: Common Files -->
+
+<!--
+    ===================
+        START: HEAD FILES   
+    ===================
+--> 
